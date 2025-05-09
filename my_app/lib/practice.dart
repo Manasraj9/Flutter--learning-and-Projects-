13bfc0e0d7f54a76203fc573b37a1ec560a53c2d
Manas Raj <manasraj1357@gmail.com>
2025-05-09 22:14:59 +0530

    day 2

--- my_app/lib/practice.dart
+++ my_app/lib/practice.dart
@@ -1,7 +1,17 @@
 import 'dart:io';
 void main(){
   // print('Welcome to Dart!');
-  stdout.write('Welcome to Dart!');
-  var name = stdin.readLineSync();
-  print("Welcome, $name");
+  // stdout.write('Welcome to Dart!');
+  // var name = stdin.readLineSync();
+  // print("Welcome, $name");
+  Human();
+
+  //Declaration of Variable
+  int? a;  //Nullable variable
+
+  print(a);
+}
+
+class Human{
+  Human();
 }
